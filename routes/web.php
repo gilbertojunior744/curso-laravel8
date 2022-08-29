9eb715036035fc5965d0f4e1759180ec6229e4e3
Gilberto Junior <gilbertojunior744@gmail.com>
2022-08-29 07:16:22 -0300

    criação CRUD

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -7,3 +8,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts',[PostController::class,'index'])->name('posts.index'); 
+
+Route::get('/posts/create',[PostController::class,'create'])->name('posts.create');
+
+Route::post('/posts',[PostController::class,'store'])->name('posts.store');
+
+Route::get('/posts/{id}',[PostController::class,'show'])->name('posts.show'); 
+
+Route::delete('/posts/{id}',[PostController::class,'delete'])->name('posts.delete');
+
+Route::get('/posts/edit/{id}',[PostController::class,'edit'])->name('posts.edit');
+
+Route::put('/posts/{id}',[PostController::class,'update'])->name('posts.update');
